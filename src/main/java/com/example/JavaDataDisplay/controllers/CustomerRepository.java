73623c413a672fbbc2610fb4040cddf9c911bfa9
Igor Figueiredo <igfigueiredo@outlook.com>
2022-02-21 18:06:31 +0100

    "ConnectionClass and CustomerRepository created"

---
+++ src/main/java/com/example/JavaDataDisplay/controllers/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.example.JavaDataDisplay.controllers;
+import java.sql.Connection;
+
+public class CustomerRepository {
+    private String URL = ConnectionHelper.CONNECTION_URL;
+    private Connection conn = null;
+}
