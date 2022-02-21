082c3607e0ab0c4689fc542c9446b41731c522c0
Dianto <diantobosman1@gmail.com>
2022-02-21 13:54:56 +0100

    Created the Customer class which will be needed to store the Strings retrieved from the database.

---
+++ src/main/java/com/example/JavaDataDisplay/models/Customer.java
@@ -0,0 +1,78 @@
+package com.example.JavaDataDisplay.models;
+
+public class Customer {
+    private String customerId;
+    private String firstName;
+    private String lastName;
+    private String county;
+    private String postalCode;
+    private String phoneNumber;
+    private String email;
+
+    public Customer(String customerId, String firstName, String lastName, String country, String postalCode, String phoneNumber, String email){
+        this.customerId = customerId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.county = country;
+        this.postalCode = postalCode;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    // Getters and Setters
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCounty() {
+        return county;
+    }
+
+    public void setCounty(String county) {
+        this.county = county;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
