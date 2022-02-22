71b75bb44d97adbc60bbaf669c3c2a624ea9d7e6
Igor Figueiredo <igfigueiredo@outlook.com>
2022-02-22 22:01:31 +0100

    CustomerController created fof Thymeleaf view

--- src/main/java/com/example/JavaDataDisplay/JavaDataDisplayApplication.java
+++ src/main/java/com/example/JavaDataDisplay/JavaDataDisplayApplication.java
@@ -1,5 +1,4 @@
 package com.example.JavaDataDisplay;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/example/JavaDataDisplay/view/CustomerController.java
+++ src/main/java/com/example/JavaDataDisplay/view/CustomerController.java
@@ -1,12 +1,14 @@
 package com.example.JavaDataDisplay.view;
 
+import com.example.JavaDataDisplay.dataAcces.CustomerRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-// Deze nog in orde maken.
 @Controller
-public class HomeController {
+public class CustomerController {
+    private CustomerRepository customerRepository = new CustomerRepository();
+
     @GetMapping("/customers")
     public String getAllCustomers(Model model){
         model.addAttribute("customers", customerRepository.getAllCustomers());
