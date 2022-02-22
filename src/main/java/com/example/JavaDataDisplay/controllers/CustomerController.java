6c9c5b5da6efe241c02ccbe04ab581c5e24f0a22
Dianto <diantobosman1@gmail.com>
2022-02-22 11:04:22 +0100

    Created the html files to add customers and to view customers. They are not working yet.

--- src/main/java/com/example/JavaDataDisplay/controllers/CustomerController.java
+++ src/main/java/com/example/JavaDataDisplay/controllers/CustomerController.java
@@ -1,75 +1,49 @@
 package com.example.JavaDataDisplay.controllers;
 import com.example.JavaDataDisplay.models.Customer;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.ArrayList;
-import com.example.JavaDataDisplay.controllers.CustomerRepository;
 
-
-@RestController
+@Controller
 public class CustomerController {
-    // We pass all the work to the repository
-    private CustomerRepository customerRepository = new CustomerRepository();
+ /*
+  "GET /"
+  "GET /customers"
+ */
 
-    @RequestMapping(value = "/api/customers", method = RequestMethod.GET)
-    public ArrayList<Customer> getAllCustomers() {
-        return customerRepository.getAllCustomers();
+    CustomerRepository crep = new CustomerRepository();
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
     }
 
-    /*
-  This returns a specific customer, based on a given Id.
-  We use a query string here to extract the Id.
-  E.g. api/customer?id=ALFKI
-  Seen as a BAD practice
-*/
-
-    @RequestMapping(value = "api/customer", method = RequestMethod.GET)
-    public Customer getCustomerByQueryId(@RequestParam(value="id", defaultValue = "ALFKI") String id){
-        return customerRepository.getCustomerById(id);
+    @GetMapping("/customers")
+    public String getAllCustomers(Model model) {
+        model.addAttribute("customers", crep.getAllCustomers());
+        return "view-customers_old";
     }
 
-    /*
-  This returns a specific customer, based on a given Id.
-  We use a header here to extract the Id.
-*/
-
-    @RequestMapping(value = "api/customerheader", method = RequestMethod.GET)
-    public Customer getCustomerByHeaderId(@RequestHeader("id") String id){
-        return customerRepository.getCustomerById(id);
+    @GetMapping("/customers/register")
+    public String showAddCustomer(Model model){
+        model.addAttribute("customer", new Customer("","","","","","",""));
+        return "add-customers";
     }
 
-    /*
-  This returns a specific customer, based on a given Id.
-  We use a path variable here to extract the Id.
-*/
-
-    @RequestMapping(value = "api/customers/{id}", method = RequestMethod.GET)
-    public Customer getCustomerByPathId(@PathVariable String id){
-        return customerRepository.getCustomerById(id);
+    @PostMapping("/customers/register")
+    public String handleAddCustomer(@ModelAttribute Customer customer, BindingResult errors, Model model){
+        boolean success = crep.addCustomer(customer);
+        if(success){
+            model.addAttribute("customer", new Customer("","","","","","",""));
+        } else {
+            model.addAttribute("customer", customer);
+        }
+        model.addAttribute("success", success);
+        return "add-customers";
     }
-
-    /*
-  This adds a new customer.
-  It takes the new customer from the body of the request.
-*/
-
-    @RequestMapping(value = "api/customers", method = RequestMethod.POST)
-    public Boolean addNewCustomer(@RequestBody Customer customer){
-        return customerRepository.addCustomer(customer);
-    }
-
-    /*
-  This updates an existing customer.
-  It takes the new customer data from the body of the request.
-  As well as taking the Id of the customer from the path variables, this is
-  to do a check if the body matches the id. Just a layer of saftey.
-*/
-
-    @RequestMapping(value = "api/customers/{id}", method = RequestMethod.PUT)
-    public Boolean updateExistingCustomer(@PathVariable String id, @RequestBody Customer customer){
-        return customerRepository.updateCustomer(customer);
-    }
-
-
 
 }
